21f646ae982946ebf45f68cc4892b03c7f97fa0c
evi1 <w1zirn1996@gmail.com>
2019-03-02 13:50:32 +0800

    scroll log

--- log.js
+++ log.js
@@ -18,8 +18,15 @@ ipc.on('newLog', (event, arg) => {
 });
 
 $(".card-body").html("");
+let card = $(".card")[0];
+card.scrollTop = card.scrollHeight - card.clientHeight;
 
 function appendLog(log) {
     let h = $(".card-body").html();
+    let card = $(".card")[0];
+    let b = card.clientHeight + card.scrollTop >= card.scrollHeight - 2;
     $(".card-body").html(h + `<p>${log}</p>`);
+    if (b) {
+        card.scrollTop = card.scrollHeight - card.clientHeight;
+    }
 }
